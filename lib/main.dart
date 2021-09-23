9de4eafdb2c885d69f5316e0809d2c6ae4b874ba
MohamedSalama0 <88866544+MohamedSalama0@users.noreply.github.com>
2021-09-23 19:33:38 +0200

    Suntastic

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'View/home_screen.dart';
+import 'View/login_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Suntastic',
+      home: AnimatedSplashScreen(
+        duration: 2000,
+        splash: Image.asset('images/SunSplash.jpg',fit: BoxFit.cover,),
+        splashIconSize: double.infinity,
+        splashTransition: SplashTransition.decoratedBoxTransition,
+        pageTransitionType: PageTransitionType.bottomToTop,
+        nextScreen: LoginScreen(),
+      ),
+    );
+  }
+}
